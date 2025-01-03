6df87d1dd71659c3bdd59e306727940ec31d951c
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-01-03 10:22:59 -0600

    chore(deps): locks pnpm deps

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -1,10 +1,10 @@
 import type { motion } from 'framer-motion';
 import type React from 'react';
 
-export type PropsWithoutRef<T extends HTMLIntrinsicElements> =
-  React.ComponentPropsWithoutRef<
-    (typeof motion)[T extends keyof typeof motion ? T : never]
-  >;
+export type PropsWithoutRef<T extends keyof React.JSX.IntrinsicElements> =
+  T extends keyof typeof motion
+    ? React.ComponentPropsWithoutRef<(typeof motion)[T]>
+    : React.ComponentPropsWithoutRef<T>;
 
 export type HTMLIntrinsicElements = keyof React.JSX.IntrinsicElements;
 

de6ae223daf635ec0bc9fe860f233e2a5b2069e4
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-01-20 10:54:01 -0600

    feat(ui): adds Typescript Styleguide project (#40)

--- app/config/projects.ts
+++ app/config/projects.ts
@@ -4,12 +4,13 @@ import dashboardPreview from '~/images/dribbble/dashboard_preview.png';
 import eyeSeeYou from '~/images/dribbble/eye_see_you.png';
 import liquiFly from '~/images/dribbble/liqui-fly.png';
 import octopus from '~/images/dribbble/octopus.png';
-import portfolioMockup from '~/images/dribbble/portfolio_mockup_v1.0.png';
 import spotonApp from '~/images/dribbble/spoton_app_sample.png';
 import spotonDiscovery from '~/images/dribbble/spoton_discovery.png';
 import spotonWebsite from '~/images/dribbble/spoton_website.png';
 import vividSeatsCheckout from '~/images/dribbble/vivid_seats_checkout_concept.png';
 import warriorSummitWebsite from '~/images/dribbble/warrior_summit_website.png';
+import portfolioMockup from '~/images/projects/portfolio_mockup_v1.0.png';
+import typescriptStyleguideCover from '~/images/projects/typescript_styleguide_cover.png';
 
 export const uiProjectImages: Array<DribbbleShot> = [
   {
@@ -60,18 +61,19 @@ export const uiProjectImages: Array<DribbbleShot> = [
 
 export const frontEndProjects: Array<FrontEndProject> = [
   {
-    imgUrl: portfolioMockup,
-    title: 'Styleguide',
-    description: 'description',
+    title: 'Typescript Styleguide',
+    url: 'https://kurocado-studio.github.io/styleguide/starter-topic.html',
+    description:
+      'Aim to streamline development setup in 5 minutes or less, facilitate easier collaboration, enhance the overall quality of software products, and improving collaboration processes between teams.',
+    imgUrl: typescriptStyleguideCover,
     categories: [],
-    url: '',
   },
   {
-    imgUrl: portfolioMockup,
     title: 'Exploring Motion & Design',
+    url: 'https://github.com/Kurocado-Studio/website',
     description:
       'This portfolio website highlights my experimentation with Framer Motion, demonstrating smooth, dynamic transitions and animations that bring the user experience to life. Built with Remix, React and Tailwind CSS.',
+    imgUrl: portfolioMockup,
     categories: [],
-    url: 'https://github.com/Kurocado-Studio/website',
   },
 ];

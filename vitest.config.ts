522d3fa6676d3477043616cce5fd3df232674183
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-02-07 10:36:13 -0600

    test(config): adds testing framework

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+/* eslint import/no-default-export: 0 */
+
+/* eslint import/no-cycle: 0 */
+
+/**
+ * Made with ❤️ and adobo by Kurocado Studio
+ * Copyright (c) 2024. All Rights Reserved.
+ *
+ * Learn more about Kurocado Studio: {@link https://www.kurocado.studio}
+ *
+ * Explore our open-source projects: {@link https://github.com/kurocado-studio}
+ */
+import { defineConfig, vitestRemix } from '@kurocado-studio/qa';
+
+export default defineConfig(vitestRemix);
